bca403d9c7d70344123f17e19fc904962b085d2e
Nelson Lee <leelhn2345@outlook.com>
2021-11-01 01:53:57 +0800

    added graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { RootSchema } from '../schema/schema';
+
+const PORT = 4000;
+const app = express();
+
+/**
+ * GraphQL endpoint / express middlewaare
+ */
+app.use('/graphql', graphqlHTTP({ schema: RootSchema, graphiql: true }));
+
+app.listen(PORT, () => {
+  console.log(`app listening on PORT ${PORT}`);
+});
